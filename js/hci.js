1cd149e1388a1980ff6c5a1a931563d2e62b1584
sshu <shhu@student.ethz.ch>
2019-11-25 02:24:26 +0100

    update

--- js/hci.js
+++ js/hci.js
@@ -1,3 +1,30 @@
+/*
+Mouse pos indicator
+*/
+var mousePos = new Path.Circle(new Point(20, 20), 15);
+mousePos.fillColor = 'black'
+mousePos.opacity = 0.2
+
+var mouseClick = new Path.Circle(new Point(20, 20), 5);
+mouseClick.fillColor = 'black'
+mouseClick.opacity = 0
+
+function onMouseMove(event) {
+	mousePos.position = event.point;
+}
+function onMouseDown(event) {
+    mouseClick.opacity = 0.5
+    // mouseClick.radius = 40
+    mouseClick.position = event.point
+    mouseClick.onFrame = function(event) {
+        if (mouseClick.opacity > 0.01) {
+            mouseClick.opacity -= 0.01
+        } else {
+            mouseClick.opacity = 0
+        }
+    }
+}
+
 // Create a Paper.js Path to draw a line into it:
 var path = new Path();
 // Give the stroke a color
@@ -8,6 +35,48 @@ path.moveTo(start);
 // Note the plus operator on Point objects.
 // PaperScript does that for us, and much more!
 path.lineTo(start + [ 100, -50 ]);
+
+var mp = new Path();
+mp.strokeColor = 'black';
+var mpoi = new Point(10, 20);
+mp.add(mpoi);
+var mpoi = new Point(200, 20);
+mp.add(mpoi);
+var mpoi = new Point(200, 300);
+mp.add(mpoi);
+
+var rect = new Path.Rectangle(new Point(20,20), new Point(80, 80));
+rect.strokeColor = 'black';
+rect.fillColor = 'black';
+
+var text = new PointText(new Point(200, 50));
+text.justification = 'center';
+text.fillColor = 'black';
+text.content = 'The contents of the point text';
+text.visible = false;
+
+
+rect.onMouseEnter = function() {
+    text.visible = true;
+}
+rect.onMouseLeave = function() {
+    text.visible = false;
+}
+
+
+
+var cc = new Path.Circle(
+    {
+        center: view.center,
+        radius: 30,
+        strokeColor: 'black'
+    }
+);
+
+function onResize(event) {
+    cc.position = view.center;
+    path.position = view.center;
+}
 
 /*
 Animated star example
