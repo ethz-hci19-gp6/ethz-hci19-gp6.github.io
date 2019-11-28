a5d1a6f8157426055e6703a1a13a7b32694c337e
sshu <shhu@student.ethz.ch>
2019-11-28 11:45:59 +0100

    add some data aggregation

--- utils/log_analysis.rb
+++ utils/log_analysis.rb
@@ -30,4 +30,24 @@ records.each do |u, s|
         r[u][sn] = (t["end"] - t["start"])
     end
 end
+
+a = {}
+r.each do |u, ss|
+    ss.each {|sn, t| if !a.has_key?(sn) then a[sn] = [t] else a[sn] << t end}
+end
+
+pr = {}
+a.each do |e, ts|
+    prn, v, interface = e.split('_')
+    if !pr.has_key?(prn) then pr[prn] = {} end
+    if !pr[prn].has_key?(interface) then pr[prn][interface] = {} end
+    if !pr[prn][interface].has_key?(v) then pr[prn][interface][v] = [] end
+    ts.each {|t| pr[prn][interface][v] << t}
+end
+
+# user wise
 ap r
+# event wise
+ap a
+# problem wise
+ap pr
